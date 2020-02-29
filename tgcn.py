f5583c07468fc1f7d8875ee317ca1c374a9af419
xumwen <421269175@qq.com>
2020-02-29 16:20:23 +0800

    update tgcn with enc-dec structure

--- tgcn.py
+++ tgcn.py
@@ -15,7 +15,6 @@ class GCNBlock(nn.Module):
                             out_channels=spatial_channels)
         self.gcn2 = GCNCell(in_channels=spatial_channels,
                             out_channels=spatial_channels)
-        # self.batch_norm = nn.BatchNorm2d(num_nodes)
         
     def forward(self, X, A):
         """
@@ -32,13 +31,48 @@ class GCNBlock(nn.Module):
 
         return out
 
-class GRUBlock(nn.Module):
+class EncoderGRU(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderGRU, self).__init__()
+        self.hidden_size = hidden_size
+        self.gru = torch.nn.GRU(input_size, hidden_size)
+    
+    def forward(self, X):
+        """
+        :param X: Input data of shape (input_seq_len, batch_size, hidden_size).
+        :return output: Output data of shape (input_seq_len, batch_size, hidden_size).
+        :return hidden: Last hidden of shape (1, batch_size, hidden_size)
+        """
+        hidden = torch.zeros(1, X.shape[1], self.hidden_size, device=X.device)
+        output, hidden = self.gru(X, hidden)
+        return output, hidden
+
+class DecoderGRU(nn.Module):
     def __init__(self, input_size, hidden_size, output_seq_len):
+        super(DecoderGRU, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_seq_len = output_seq_len
+        self.gru_cell = torch.nn.GRUCell(input_size, hidden_size)
+    
+    def forward(self, X, hidden):
+        """
+        :param X, hidden: Encoder's last hidden of shape (batch_size, hidden_size).
+        :return: Output data of shape (batch_size, output_seq_len, hidden_size).
+        """
+        output = torch.zeros(self.output_seq_len, X.shape[0], self.hidden_size, device=X.device)
+        for i in range(self.output_seq_len):
+            hidden = self.gru_cell(X, hidden)
+            output[i] = hidden
+        return output.permute(1, 0, 2)
+    
+class GRUBlock(nn.Module):
+    def __init__(self, input_size, hidden_size, input_seq_len, output_seq_len):
         super(GRUBlock, self).__init__()
         self.hidden_size = hidden_size
         self.output_seq_len = output_seq_len
-        self.gru = torch.nn.GRU(input_size, hidden_size)
-
+        self.encoder_gru = EncoderGRU(input_size, hidden_size)
+        self.decoder_gru = DecoderGRU(hidden_size, hidden_size, output_seq_len)
+        
     def forward(self, X):
         """
         :param X: Input data of shape (batch_size, num_nodes, input_seq_len,
@@ -48,14 +82,10 @@ class GRUBlock(nn.Module):
         """
         # batch_size * num_nodes -> batch
         gru_input = X.contiguous().view(-1, X.shape[2], X.shape[3]).permute(1, 0, 2)
-        hidden = torch.zeros(1, X.shape[0] * X.shape[1], self.hidden_size, device=X.device)
-        t, hidden = self.gru(gru_input, hidden)
-        output = torch.zeros(self.output_seq_len, gru_input.shape[1], self.hidden_size, device=X.device)
-        for i in range(self.output_seq_len):
-            t, hidden = self.gru(t, hidden)
-            output[i] = t[0]
-        # batch_size -> (batch_size, num_nodes)
-        output = output.permute(1, 0, 2).view(X.shape[0], X.shape[1], self.output_seq_len, self.hidden_size)
+        t, hidden = self.encoder_gru(gru_input)
+        output = self.decoder_gru(t[-1], t[-1])  
+        # batch -> batch_size * num_nodes
+        output = output.view(X.shape[0], X.shape[1], self.output_seq_len, self.hidden_size)
         return output
 
 class TGCN(nn.Module):
@@ -70,10 +100,10 @@ class TGCN(nn.Module):
         output by the network.
         """
         super(TGCN, self).__init__()
-        # self.input_linear = nn.Linear(num_features, hidden_size)
         self.gcn = GCNBlock(in_channels=num_features, spatial_channels=hidden_size,
                                 num_nodes=num_nodes, gcn_type=gcn_type)
         self.gru = GRUBlock(input_size=hidden_size, hidden_size=hidden_size,
+                                input_seq_len = num_timesteps_input,
                                 output_seq_len=num_timesteps_output)
         self.linear = nn.Linear(hidden_size, 1)
 
@@ -84,7 +114,6 @@ class TGCN(nn.Module):
         :param A_hat: Normalized adjacency matrix.
         """
         out1 = self.gcn(X, A)
-        # out1 = self.input_linear(X)
         out2 = self.gru(out1)
         out3 = self.linear(out2).squeeze(dim = 3)
         return out3

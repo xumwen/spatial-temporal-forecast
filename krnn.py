8bb3a53f059b02242ffcda0aad376e951d576330
weicao <weicao@microsoft.com>
2020-03-10 01:17:08 +0800

    add en-de

--- krnn.py
+++ krnn.py
@@ -17,15 +17,21 @@ class KRNN(nn.Module):
         output by the network.
         """
         super(KRNN, self).__init__()
-        self.grus = nn.ModuleList()
+        self.encoders = nn.ModuleList()
+        self.decoders = nn.ModuleList()
         self.linears = nn.ModuleList()
 
+        self.num_timesteps_output = num_timesteps_output
+
         for r in range(num_comps):
-            self.grus.append(
+            self.encoders.append(
                 nn.GRU(num_features, hidden_size)
             )
+            self.decoders.append(
+                nn.GRUCell(1, hidden_size)
+            )
             self.linears.append(
-                nn.Linear(hidden_size, num_timesteps_output)
+                nn.Linear(hidden_size, 1)
             )
 
         self.embed = nn.Parameter(torch.FloatTensor(num_nodes, num_comps))
@@ -44,13 +50,24 @@ class KRNN(nn.Module):
         sz = X.size()
         X = X.view(-1, sz[2], sz[3]).permute(1, 0, 2)
 
-        for i in range(len(self.grus)):
-            hid, _ = self.grus[i](X)
-            hid = hid.mean(dim=0)
-            hid = hid.view(sz[0], sz[1], -1)
-            out.append(
-                self.linears[i](hid).unsqueeze(dim=-1)
-            )
+        for i in range(len(self.encoders)):
+            encoder_out, encoder_hid = self.encoders[i](X)
+            decoder_out = []
+
+            last_value = X[-1, :, 0].view(-1, 1)
+            decoder_hid = encoder_hid.squeeze(dim=0)
+
+            for step in range(self.num_timesteps_output):
+                decoder_hid = self.decoders[i](last_value, decoder_hid)
+                value = self.linears[i](decoder_hid)
+
+                decoder_out.append(value)
+
+                last_value = value
+
+            decoder_out = torch.cat(decoder_out, dim=-1).view(sz[0], sz[1], self.num_timesteps_output)
+
+            out.append(decoder_out.unsqueeze(dim=-1))
         
         out = torch.cat(out, dim=-1)
         weight = torch.softmax(self.embed, dim=-1)

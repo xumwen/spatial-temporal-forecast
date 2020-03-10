46c666849e4398bc9d8644f762d4fdd8535b2621
xumwen <421269175@qq.com>
2020-03-10 20:51:45 +0800

    test gcn with PyG

---
+++ stgcn_pyg.py
@@ -0,0 +1,132 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, ChebConv
+
+
+class TimeBlock(nn.Module):
+    """
+    Neural network block that applies a temporal convolution to each node of
+    a graph in isolation.
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size=3):
+        """
+        :param in_channels: Number of input features at each node in each time
+        step.
+        :param out_channels: Desired number of output channels at each node in
+        each time step.
+        :param kernel_size: Size of the 1D temporal kernel.
+        """
+        super(TimeBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, (1, kernel_size))
+        self.conv2 = nn.Conv2d(in_channels, out_channels, (1, kernel_size))
+        self.conv3 = nn.Conv2d(in_channels, out_channels, (1, kernel_size))
+
+    def forward(self, X):
+        """
+        :param X: Input data of shape (batch_size, num_nodes, num_timesteps,
+        num_features=in_channels)
+        :return: Output data of shape (batch_size, num_nodes,
+        num_timesteps_out, num_features_out=out_channels)
+        """
+        # Convert into NCHW format for pytorch to perform convolutions.
+        X = X.permute(0, 3, 1, 2)
+        temp = self.conv1(X) + torch.sigmoid(self.conv2(X))
+        out = F.relu(temp + self.conv3(X))
+        # Convert back from NCHW to NHWC
+        out = out.permute(0, 2, 3, 1)
+        return out
+
+
+class STGCNBlock(nn.Module):
+    """
+    Neural network block that applies a temporal convolution on each node in
+    isolation, followed by a graph convolution, followed by another temporal
+    convolution on each node.
+    """
+
+    def __init__(self, in_channels, spatial_channels, out_channels,
+                 num_nodes, gcn_type):
+        """
+        :param in_channels: Number of input features at each node in each time
+        step.
+        :param spatial_channels: Number of output channels of the graph
+        convolutional, spatial sub-block.
+        :param out_channels: Desired number of output features at each node in
+        each time step.
+        :param num_nodes: Number of nodes in the graph.
+        """
+        super(STGCNBlock, self).__init__()
+        self.temporal1 = TimeBlock(in_channels=in_channels,
+                                   out_channels=out_channels)
+        if gcn_type == 'cheb':
+            GCNCell = ChebConv
+        else:
+            GCNCell = GCNConv
+        self.gcn = GCNCell(in_channels=out_channels,
+                           out_channels=spatial_channels, node_dim=1)
+        self.temporal2 = TimeBlock(in_channels=spatial_channels,
+                                   out_channels=out_channels)
+        self.batch_norm = nn.BatchNorm2d(num_nodes)
+
+    def forward(self, X, edge_index, edge_weight=None):
+        """
+        :param X: Input data of shape (batch_size, num_nodes, num_timesteps,
+        num_features=in_channels).
+        :param A_hat: Normalized adjacency matrix.
+        :return: Output data of shape (batch_size, num_nodes, num_timesteps_out,
+        num_features=out_channels).
+        """
+        t1 = self.temporal1(X)
+        # batch_size * timesteps -> batch_size
+        t21 = t1.permute(0, 2, 1, 3).contiguous().view(-1, t1.shape[1], t1.shape[3])
+        t22 = F.relu(self.gcn(t21, edge_index, edge_weight))
+        # batch_size -> (batch_size, timesteps)
+        t23 = t22.view(t1.shape[0], t1.shape[2], t22.shape[1], t22.shape[2]).permute(0, 2, 1, 3)
+        t3 = self.temporal2(t23)
+        
+        return self.batch_norm(t3)
+
+
+class STGCN(nn.Module):
+    """
+    Spatio-temporal graph convolutional network as described in
+    https://arxiv.org/abs/1709.04875v3 by Yu et al.
+    Input should have shape (batch_size, num_nodes, num_input_time_steps,
+    num_features).
+    """
+
+    def __init__(self, num_nodes, num_features, num_timesteps_input,
+                 num_timesteps_output, gcn_type='normal'):
+        """
+        :param num_nodes: Number of nodes in the graph.
+        :param num_features: Number of features at each node in each time step.
+        :param num_timesteps_input: Number of past time steps fed into the
+        network.
+        :param num_timesteps_output: Desired number of future time steps
+        output by the network.
+        """
+        super(STGCN, self).__init__()
+        self.block1 = STGCNBlock(in_channels=num_features, out_channels=64,
+                                 spatial_channels=16, num_nodes=num_nodes,
+                                 gcn_type=gcn_type)
+        self.block2 = STGCNBlock(in_channels=64, out_channels=64,
+                                 spatial_channels=16, num_nodes=num_nodes,
+                                 gcn_type=gcn_type)
+        self.last_temporal = TimeBlock(in_channels=64, out_channels=64)
+        self.fully = nn.Linear((num_timesteps_input - 2 * 5) * 64,
+                               num_timesteps_output)
+
+    def forward(self, X, edge_index, edge_weight=None):
+        """
+        :param X: Input data of shape (batch_size, num_nodes, num_timesteps,
+        num_features=in_channels).
+        :param A: Normalized adjacency matrix.
+        """
+        out1 = self.block1(X, edge_index, edge_weight)
+        out2 = self.block2(out1, edge_index, edge_weight)
+        out3 = self.last_temporal(out2)
+        out4 = self.fully(out3.reshape((out3.shape[0], out3.shape[1], -1)))
+        return out4

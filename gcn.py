d9abfc5bf8ce3008852f7d53ea13b09541ea4552
xumwen <421269175@qq.com>
2020-03-20 00:10:55 +0800

    Fix bug in ours' ChebConv's norm

--- gcn.py
+++ gcn.py
@@ -3,6 +3,8 @@ import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
+import torch_geometric.nn as PyG
+from torch_geometric.data import Data, Batch, DataLoader
 
 class GCNConv(nn.Module):
     """
@@ -73,10 +75,10 @@ class ChebConv(nn.Module):
         Returns the normalized and scaled adjacency matrix.
         Formula: L = I - D^(-1/2) * A * D^(-1/2), L_hat = (2L / lambda_max) - I
         """
-        I = torch.ones(A.shape[0]).to(A.device)
+        I = torch.diag(torch.ones(A.shape[0])).to(A.device)
         # Remove self-loops
-        if A[0][0] != 0:
-            A = A - torch.diag(A[0][0] * I)
+        A = A - A[0][0] * I
+        
         D = A.sum(1).view((-1,))
         D[D <= 10e-5] = 10e-5
         diag = torch.reciprocal(torch.sqrt(D))
@@ -105,4 +107,105 @@ class ChebConv(nn.Module):
             out = out + torch.matmul(Tx_2, self.weight[k])
             Tx_0, Tx_1 = Tx_1, Tx_2
 
+        return out
+
+
+class PyGConv(nn.Module):
+    """
+    Choose GCN implemented by pytorch-geometric and apply to a batch of nodes.
+    """
+    def __init__(self, in_channels, out_channels, gcn_type):
+        """
+        :param in_channels: Number of input features at each node.
+        :param out_channels: Desired number of output channels at each node.
+        :param gcn_type: Choose GCN type.
+        """
+        super(PyGConv, self).__init__()
+
+        # Use edge_weight argument in forward
+        self.adj_available = True
+        self.batch_training = False
+        self.kwargs = {'in_channels':in_channels, 'out_channels':out_channels}
+
+        if gcn_type == 'gat':
+            self.adj_available = False
+        if gcn_type in ['normal', 'cheb', 'graph']:
+            self.batch_training = True
+            self.kwargs['node_dim'] = 1
+        if gcn_type == 'cheb':
+            self.kwargs['K'] = 3
+        
+        GCNCell = {'normal':PyG.GCNConv, 
+                    'cheb':PyG.ChebConv,
+                    'sage':PyG.SAGEConv, 
+                    'graph':PyG.GraphConv,
+                    'gat':PyG.GATConv}\
+                    .get(gcn_type)
+        
+        self.gcn = GCNCell(**self.kwargs)
+    
+    def get_batch(self, X):
+        # Wrap input node and edge features, along with the single edge_index, into a `torch_geometric.data.Batch` instance
+        data_list = [Data(x=x) for x in X]
+
+        return Batch.from_data_list(data_list)
+
+    def forward(self, X, edge_index, edge_weight):
+        """
+        :param X: Input data of shape (batch_size, num_nodes, in_channels)
+        :param edge_index: Graph connectivity in COO format with shape(2, num_edges)
+        :param edge_weight: Edge feature matrix with shape (num_edges, num_edge_features)
+        :return: Output data of shape (batch_size, num_nodes, out_channels)
+        """
+        if self.batch_training:
+            if self.adj_available:
+                out = self.gcn(X, edge_index, edge_weight)
+            else:
+                out = self.gcn(X, edge_index)
+        else:
+            # Currently, conv in [SAGEConv, GATConv] cannot use argument node_dim for batch training
+            # This is a temp solution but it's very very very slow!
+            batch = self.get_batch(X)
+            if self.adj_available:
+                out = self.gcn(batch.x, edge_index, edge_weight)
+            else:
+                out = self.gcn(batch.x, edge_index)
+        
+        return out.view(X.shape[0], X.shape[1], -1)
+
+class GCNUnit(nn.Module):
+    """
+    Choose GCNUnit with package and type.
+    """
+    def __init__(self, in_channels, out_channels, gcn_type, gcn_package):
+        """
+        :param in_channels: Number of input features at each node.
+        :param out_channels: Desired number of output channels at each node.
+        :param gcn_type: Choose GCN type.
+        :param gcn_package: Choose GCN package in ['pyg', 'ours'].
+        """
+        super(GCNUnit, self).__init__()
+        self.adj_type = 'sparse'
+        if gcn_package == 'pyg':
+            self.gcn = PyGConv(in_channels=in_channels,
+                                out_channels=out_channels,
+                                gcn_type=gcn_type)
+        else:
+            self.adj_type = 'dense'
+            GCNCell = {'normal':GCNConv, 'cheb':ChebConv}\
+                .get(gcn_type)
+            self.gcn = GCNCell(in_channels=in_channels,
+                                out_channels=out_channels)
+
+    def forward(self, X, A=None, edge_index=None, edge_weight=None):
+        """
+        :param X: Input data of shape (batch_size, num_nodes, in_channels)
+        :param **kwargs: Additional arguments(dense or sparse adj matrix).
+        :return: Output data of shape (batch_size, num_nodes, out_channels)
+        """
+        if self.adj_type == 'sparse':
+            out = self.gcn(X, edge_index=edge_index, edge_weight=edge_weight)
+        else:
+            out = self.gcn(X, A=A)
+        
         return out

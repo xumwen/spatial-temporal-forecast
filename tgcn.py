6aed2d95f1db92557fcfa62ecb3806588966fb52
xumwen <421269175@qq.com>
2020-04-15 15:34:30 +0800

    remove linear before gcn

--- tgcn.py
+++ tgcn.py
@@ -8,8 +8,7 @@ class GCNBlock(nn.Module):
     def __init__(self, in_channels, spatial_channels, num_nodes, 
                 gcn_type, gcn_package, gcn_partition):
         super(GCNBlock, self).__init__()
-        self.linear = nn.Linear(in_channels, spatial_channels)
-        self.gcn1 = GCNUnit(in_channels=spatial_channels,
+        self.gcn1 = GCNUnit(in_channels=in_channels,
                             out_channels=spatial_channels,
                             gcn_type=gcn_type,
                             gcn_package=gcn_package,
@@ -29,7 +28,6 @@ class GCNBlock(nn.Module):
         num_timesteps_out, num_features=out_channels).
         """
         t1 = X.permute(0, 2, 1, 3).contiguous().view(-1, X.shape[1], X.shape[3])
-        t1 = self.linear(t1)
         t2 = F.relu(self.gcn1(t1, A, edge_index, edge_weight))
         t3 = torch.sigmoid(self.gcn2(t2, A, edge_index, edge_weight))
         out = t3.view(X.shape[0], X.shape[2], t3.shape[1], t3.shape[2]).permute(0, 2, 1, 3)
